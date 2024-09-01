d2da2bb508f0d6577306275745d41d7ccc28d01a
Wkayaobama <118966619+Wkayaobama@users.noreply.github.com>
2024-09-01 15:20:05 +0200

    Update embedchain_rag.py

--- embedchain_rag.py
+++ embedchain_rag.py
@@ -1,18 +1,1 @@
-import config
-from embedchain import App
-import os
 
-
-os.environ["OPENAI_API_KEY"] = OpenAI(api_key=config.API_KEY)
-def create_embedchain_app():
-    return App
-
-def add_url_to_embedchain(app, url):
-    if url:
-        app.add("web_page", url)
-
-def add_file_to_embedchain(app, file_content):
-    app.add("file_type", file_content)  # Replace 'file_type' with actual supported type
-
-def process_embedchain_data(app):
-    return app.process()  # Assuming there is a process method
